d6557d827b506fd924b6d58d612d381b8fd1f1ac
cnatanae <cnatanae@student.42sp.org.br>
2023-11-17 11:51:37 -0300

    Leaks bugs fix

--- src/get_next_line.c
+++ src/get_next_line.c
@@ -6,153 +6,102 @@
 /*   By: cnatanae <cnatanae@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 12:53:15 by cnatanae          #+#    #+#             */
-/*   Updated: 2023/11/14 18:09:49 by cnatanae         ###   ########.fr       */
+/*   Updated: 2023/11/17 11:37:48 by cnatanae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-t_char	*ft_node(char c)
-{
-	t_char	*new_node;
-	new_node = malloc(sizeof(t_char));
-	if (!new_node)
-	{
-		printf("erro 1");
-		return (NULL);
-	}
-	new_node->character = c;
-	new_node->next = NULL;
-	return (new_node);
-}
-
-int	ft_lstsize(t_char *lst, t_list *struc)
-{
-	int	idx;
-
-	idx = 0;
-	while (lst->character != '\n' || !lst)
-	{
-		lst = lst->next;
-		idx++;
-	}
-	struc->len = idx;
-	if (lst->character == '\n')
-	{
-		struc->len++;
-	}
-	
-	return (idx);
-}
-
 char	*ft_build_string(t_list *struc)
 {
-	size_t	size_lk;
-	char	*line;
-	int		idx;
+	size_t				size_lk;
+	char				*line;
+	unsigned long int	idx;
+	t_char				*aux;
 
 	idx = 0;
 	size_lk = ft_lstsize(struc->string, struc);
-	line = malloc(sizeof(char) * size_lk + 1);
+	line = malloc(sizeof(char) * size_lk + 2);
 	if (!line)
 	{
-		printf("erro 2");
+		free_all(&struc->string);
 		return (NULL);
 	}
 	while (struc->string && struc->string->character != '\n')
 	{
+		aux = struc->string;
 		line[idx++] = struc->string->character;
 		struc->string = struc->string->next;
+		free (aux);
 	}
-	struc->string = struc->string->next;
+	aux = struc->string;
+	if (struc->string)
+		struc->string = struc->string->next;
+	if (aux && aux->character == '\n')
+		line[idx++] = '\n';
+	free (aux);
 	line[idx] = '\0';
 	return (line);
 }
 
-int	ft_find_n(t_list *struc)
-{
-	t_char	*aux;
-
-	aux = struc->string;
-	while (aux)
-	{
-		if (aux->character == '\n')
-			return (1);
-		aux = aux->next;
-	}
-	return (0);
-}
 char	*ft_add_lk(t_list *struc)
 {
-	t_char	*aux;
-	t_char	*new_node;
-	int		exist_n;
+	t_char				*aux;
+	int					exist_n;
 
 	exist_n = 0;
-	if (!struc->string)
-		struc->string = ft_node(struc->buffer[struc->pos++]);
 	aux = struc->string;
-	while (aux->next)
+	while (aux && aux->next)
 		aux = aux->next;
-	if (!aux)
-	{
-		printf("erro 3");
-		return (NULL);
-	}
-		//exist_n = ft_find_n(struc);
-	while (exist_n == 0)
+	while (exist_n == 0 && struc->buffer[struc->pos] != '\0')
 	{
 		while (struc->buffer[struc->pos])
 		{
-			new_node = ft_node(struc->buffer[struc->pos]);
-			if (!new_node)
-			{
-				free_all(struc->string);
-				printf("erro 4");
+			aux = add_node_to_end(struc, aux, struc->buffer[struc->pos++]);
+			if (!aux)
 				return (NULL);
-			}
-			aux->next = new_node;
-			aux = aux->next;
-			struc->pos++;
 		}
+		struc->pos = 0;
 		exist_n = ft_find_n(struc);
 		if (exist_n == 0)
 		{
+			while (struc->pos <= BUFFER_SIZE)
+				struc->buffer[struc->pos++] = '\0';
 			struc->read = read(struc->fd, struc->buffer, BUFFER_SIZE);
 			struc->pos = 0;
 			if (struc->read == -1)
 			{
-				free_all(struc->string);
-				printf("erro 5");
+				free_all(&struc->string);
 				return (NULL);
 			}
 		}
 	}
-	return(ft_build_string(struc));
+	return (ft_build_string(struc));
 }
 
 char	*get_next_line(int fd)
 {
 	static t_list	struc;
-	
-	if (!fd || fd < 0 || BUFFER_SIZE <= 0)
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
 	{
-		printf("erro 6");
 		return (NULL);
 	}
 	struc.pos = 0;
 	struc.fd = fd;
+	while (struc.pos <= BUFFER_SIZE + 1)
+		struc.buffer[struc.pos++] = '\0';
+	struc.pos = 0;
 	struc.read = read(fd, struc.buffer, BUFFER_SIZE);
 	struc.buffer[struc.read] = '\0';
 	if (struc.read == -1)
 	{
-		free_all(struc.string);
-		printf("erro 7");
+		free_all(&struc.string);
 		return (NULL);
 	}
-	if (struc.string && !struc.string->character && struc.buffer[struc.pos] == '\0')
+	if (!struc.string && struc.buffer[struc.pos] == '\0')
 	{
-		free_all(struc.string);
+		free_all(&struc.string);
 		return (NULL);
 	}
 	return (ft_add_lk(&struc));
